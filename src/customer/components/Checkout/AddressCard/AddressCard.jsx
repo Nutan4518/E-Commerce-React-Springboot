14e3e8946e30e23d9be5113fcf118f18e8a23f9e
Nutan4518 <Nutan.Singh@indianpac.com>
2024-07-04 13:32:33 +0530

    product and order summary page created

---
+++ src/customer/components/Checkout/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+     <div className="space-y-3">
+      <p className='font semi-bold'> ram Singh</p>
+      <p>address ,202001</p>
+      <div className="space-y-1">
+        <p className='font semi-bold'> Phone Number</p>
+        <p >1234567890</p>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default AddressCard
